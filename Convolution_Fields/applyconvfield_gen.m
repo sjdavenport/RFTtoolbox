7f668ed319bf28fd985c9f78145adef606e72250
Samuel Davenport <davenpor@fireback.stats.ox.ac.uk>
2019-12-07 17:57:26 +0000

    backup

--- Convolution_Fields/applyconvfield_gen.m
+++ Convolution_Fields/applyconvfield_gen.m
@@ -97,7 +97,8 @@ for I = 1:size(tval, 2)
 %     field_vals(I) = sum(Y(:)'*Kernel(tval(I, :) - xvalues_at_voxels));
 
 %Matrix version of this:
-    Kernel_eval = Kernel(repmat(tval(:,I),D) - xvalues_at_voxels'); %this is the Ith tval! %Need to do this for 2016 and prior compatibility.
+    size(repmat(tval(:,I),D))
+    Kernel_eval = Kernel(repmat(tval(:,I),D, 1) - repmat(xvalues_at_voxels',1, size(tval, 2)); %this is the Ith tval! %Need to do this for 2016 and prior compatibility.
 %     Kernel_eval = Kernel(tval(:,I) - xvalues_at_voxels'); %this is the Ith tval!
     field_vals(:,I) = Kernel_eval*Y(:);
     ss(I) = sum(Kernel_eval(:).^2);
