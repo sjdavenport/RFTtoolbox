917542da718374427f9899207c154c6bbd19e37e
Fabian Telschow <ftelschow@ucsd.edu>
2020-05-20 10:47:20 +0200

    new style with reduced input arguments

--- LKC_estimators/LKCestim_HPE.m
+++ LKC_estimators/LKCestim_HPE.m
@@ -1,114 +1,211 @@
-function LKC = LKCestim_HPE( Y, D, mask, Mboot, normalize, version )
-% LKCestim_HPE( Y, D, mask, Mboot, version )
+function LKC = LKCestim_HPE( Y, mask, Mboot, normalize, version )
+% LKCestim_HPE( Y, mask, Mboot, normalize, version )
 % This function computes the Lipschitz Killing curvature using the
 % Hermite projection estimator proposed in Telschow et al (2020+).
 % It uses a fast and exact way to compute the EC curves by looping through
-% discrete critical values and computes there contribution to the change in
-% Euler characteristic.
+% discrete critical values and computes their contribution to the change in
+% the Euler characteristic.
 %--------------------------------------------------------------------------
 % ARGUMENTS
-% Y         an array of dimension T_1 x...x T_D x N of N residual
-%           fields over an T_1 x...x T_D-square.
-%           Note, it is assumed that the fields are standardized residuals
-%           or at least mean zero, if Mboot > 1.
-% D         dimension of domain of the field
-% mask      a boolean array of dimension T_1 x...x T_D. Voxels outside the
-%           mask will be set to +oo.
-% Mboot     an integer specifying the number of bootstraps used for
-%           estimation of LKC. If 1 the estimator is equal to the HPE
-%           otherwise the bHPE (recommended Mboot>3e3)
-% normalize boolean indicating whether Y is standardized. Default 1.
-% version   if "C" (default), critical values are computed using C++, if
-%           it is any other value a slow matlab only implementation is used.
+% Mandatory:
+%   Y      an array of dimension T_1 x...x T_D x N of N random
+%          fields over an T_1 x...x T_D rectangle.
+%   mask   a logical array of dimension T_1 x...x T_D. Not that the domain
+%          domain needs to be only one connected component currently in D=1. 
+%
+% Optional:
+%   Mboot     an integer specifying the number of bootstraps used for
+%             estimation of LKC. If "1" the HPE is used otherwise the
+%             bHPE. Default: Mboot 1.
+%   normalize logical indicating whether Y needs to be standardized. 
+%             Default 1, i.e., mean will be subtracted and data will be 
+%             standardized to have empirical variance 1, if N>1 else 0.
+%   version   if "C" (default), Euler characteristic is computed using a
+%             routine to compute critical values in C++, else a slow matlab
+%             only implementation is used, which is not recommended for D>2.
 %--------------------------------------------------------------------------
 % OUTPUT
-% LKC       the Lipschitz Killing curvatures of the field
+%   LKC     structure containing fields:
+%           - hatL1: D x N or D x Mboot array containing the LKC estimates
+%                    for each random field
+%           - hatL: D x 1 vector of estimates of LKC for the sample Y. It
+%                   is the average of hatL1.
+%           - L0: integer containing the Euler characteristic of the mask
+%                 equivalently the zeroth LKC of the random fields.
+%           - hatSIGMA: D x D estimate of the covariance matrix of the
+%                       estimate hatL. It is computed using the empirical
+%                       covariance of hatL1.
+%           - hatSE:    D x 1 vector of standard errors for components of
+%                       hatL based on the CLT. 
+%           - confInt95: approximate 95% confidence intervals for hatL
+%                        based on the standard CLT.
+% -------------------------------------------------------------------------
+% AUTHORS: Fabian Telschow
 %--------------------------------------------------------------------------
 % EXAMPLES
-% dim   = [50 50 50];
-% nsubj = 30;
-% D     = length( dim );
-% mask  = true(dim);
+% %%%% D = 1
+% dim   = [ 100 ];
+% mask  = ones( [dim 1] );
+% FWHM  = 12;
 % 
 % % generate random noise
-% Y = noisegen( dim, nsubj, 5*2*sqrt(2*log(2)), 0 );
-% % compute HPE and bHPE
-% HPE  = LKCestim_HPE( Y, D, mask, 1, "C" );
-% bHPE = LKCestim_HPE( Y, D, mask, 5e3, "C" );
-% -------------------------------------------------------------------------
-% AUTHOR: Fabian Telschow
+% Y = noisegen( dim, 10, FWHM, 0 );
+% 
+% % compute HPE for one subject. Note that normalize needs to be 0! It is
+% % automatically correctly chosen if not specified. All values give the same
+% % result!
+% HPE_C = LKCestim_HPE( Y(:,1), mask, 1, 0, "C" );
+% HPE_m = LKCestim_HPE( Y(:,1), mask, 1, 0, "matlab" );
+% HPE_auto = LKCestim_HPE( Y(:,1), mask );
+% 
+% % compute HPE for several subject. Here we can choose to normalize, which
+% % is recommended.
+% HPE_C = LKCestim_HPE( Y, mask, 1, 0, "C" );
+% HPE_m = LKCestim_HPE( Y, mask, 1, 1, "matlab" );
+% HPE_auto = LKCestim_HPE( Y, mask ); % same as HPE_m using "C"
+% 
+% % compute HPE for several subject. Here normalize does not have any
+% % since it is required to normalize and done automatically. Note that bHPE
+% % requires several subjects
+% bHPE_C = LKCestim_HPE( Y, mask, 1e3, 0, "C" );
+% bHPE_m = LKCestim_HPE( Y, mask, 1e3, 1, "matlab" );
+% 
+% %%%% D = 2
+% dim   = [ 50 50 ];
+% mask  = ones( dim );
+% FWHM  = sigma2FWHM(5);
+% mask(25,25) = 1;
+% 
+% % generate random noise
+% Y = noisegen( dim, 10, FWHM );
+% 
+% % compute HPE for one subject. Note that normalize needs to be 0! It is
+% % automatically correctly chosen if not specified. All values give the same
+% % result!
+% HPE_C = LKCestim_HPE( Y(:,:,1), mask, 1, 0, "C" );
+% HPE_m = LKCestim_HPE( Y(:,:,1), mask, 1, 0, "matlab" );
+% HPE_auto = LKCestim_HPE( Y(:,:,1), mask );
+% 
+% % compute HPE for several subject. Here we can choose to normalize, which
+% % is recommended, since usually it is unknown whether the observations
+% % have mean zero and unit variance.
+% HPE_C = LKCestim_HPE( Y, mask, 1, 0, "C" );
+% HPE_m = LKCestim_HPE( Y, mask, 1, 1, "matlab" );
+% HPE_auto = LKCestim_HPE( Y, mask ); % same as HPE_m using "C"
+% 
+% % compute HPE for several subject. Here normalize does not have any
+% % since it is required to normalize and done automatically. Note that bHPE
+% % requires several subjects. Note value is unexpectedly low. Need to figure
+% % out why. Using "matlab" not recommended since it is extremely slow.
+% bHPE_C = LKCestim_HPE( Y, mask, 3e3, 0, "C" );
+% bHPE_m = LKCestim_HPE( Y, mask, 1e3, 0, "matlab" );
+% 
+% %%%% D = 3
+% dim   = [ 35 35 35 ];
+% mask  = ones( dim );
+% FWHM  = 5;
+% mask(25,25,25) = 0;
+% 
+% % generate random noise
+% Y = noisegen( dim, 10, FWHM );
+% 
+% % compute HPE for one subject. Note that normalize needs to be 0! It is
+% % automatically correctly chosen if not specified. All values give the same
+% % result!
+% HPE_C = LKCestim_HPE( Y(:,:,:,1), mask, 1, 0, "C" );
+% HPE_m = LKCestim_HPE( Y(:,:,:,1), mask, 1, 0, "matlab" );
+% HPE_auto = LKCestim_HPE( Y(:,:,:,1), mask );
+% 
+% % compute HPE for several subject. Here we can choose to normalize, which
+% % is recommended.
+% HPE_C = LKCestim_HPE( Y, mask, 1, 0, "C" );
+% HPE_m = LKCestim_HPE( Y, mask, 1, 1, "matlab" );
+% HPE_auto = LKCestim_HPE( Y, mask ); % same as HPE_m using "C"
+% 
+% % compute HPE for several subject. Here normalize does not have any
+% % since it is required to normalize and done automatically. Note that bHPE
+% % requires several subjects.
+% % Never use "matlab" for bHPE since it is extremely slow.
+% bHPE_C = LKCestim_HPE( Y, mask, 3e3, 0, "C" );
+%--------------------------------------------------------------------------
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% Check input and get important constants from the mandatory input
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% Get constants from the input
+% size of the domain
+sM = size( mask );
+sY = size( Y );
 
-%%%%%%%%%%%%%%%%%%%%%%% Get constants from the input %%%%%%%%%%%%%%%%%%%%%%
-sY     = size( Y );                 % get size of the input array
-if length(sY) == D
+% dimension of the domain, since matlab is not consistent for D<1, we need
+% to catch this case 
+if sM( 2 ) == 1 && length( sM ) == 2
+    D = 1;
+else
+    D = length(sM);
+end
+
+% modify the size array of Y to keep track of N.
+if length( sY ) == D
     sY = [ sY 1 ];
 end
-N      = sY( D + 1 );               % number of subjects/samples
-index  = repmat( {':'}, 1, D );     % get variable domain counter
+% get number of subjects/samples
+N      = sY( D + 1 );
+% get variable domain counter
+index  = repmat( {':'}, 1, D );
+
+%%%% check validity of mask input
+if ~all( sM == sY( 1:end-1 ) ) && ~all( sM == sY ) && ...
+   ~( D == 1 && sM( 1 ) == sY( 1 ) )
+   error( 'The mask needs to have the same size as Y. Note that realisations are stored as columns.' )
+end
 
 % check that method is implemented for dimension D
 if D > 3
     error( 'D must be < 4. Higher dimensional domains have not been implemented')
 end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%% add default values %%%%%%%%%%%%%%%%%%%%%%%%%%%
-if ~exist( 'Mboot', 'var' ) || ~isnumeric(Mboot)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% add/check optional values
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if ~exist( 'Mboot', 'var' )
    % default number of bootstrap replicates
-   Mboot = 0;
+   Mboot = 1;
 end
 
-if exist( 'mask', 'var' )
-    % Check whether mask has the correct size and set values outside the
-    % mask to -oo
-    if( ( ~all( size( mask ) == sY( 1 : D ) ) && D ~= 1 ) || ...
-           (sY( 1 ) ~= max( size( mask ) ) && D == 1 ) )
-        error( 'Incompatible input: The mask needs to have the same size as the first D dimensions of Y.' )
+if N == 1 && Mboot > 1
+    error('The bHPE requires that N > 2!')
+end
+
+if ~exist( 'normalize', 'var' )
+    % default value of "normalize"
+    if N == 1
+       normalize = 0;
     else
-        for i = 1 : N
-            tmp = Y( index{:}, i );
-            tmp( ~mask ) = -Inf;
-            Y( index{:}, i ) = tmp;
-        end
+       normalize = 1;
     end
-else
-    % if no mask provided assume the domain is the whole image
-    mask = true( sY( 1:D ) );
 end
-clear i
 
-if nargin < 6
+if ~exist( 'version', 'var' )
     % default value of "version"
     version = "C";
 end
 
-if nargin < 5
-    % default value of "version"
-    normalize = true;
-end
 
-% prepare for parallelisation, if assumed.
-if ~isempty( str2num( version ) )
-    Npar    = str2num( version );
-    
-    % save the state of the CPU's are open already
-    state_gcp = isempty(gcp('nocreate'));
 
-    % open connection to CPUs, if not already established
-    if( state_gcp && Npar > 1 ) 
-        parpool( Npar );
-        state_gcp = 42;
-    end
-else
-    Npar    = 0;
-    state_gcp = 0;
-end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% main function %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% main function
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Initialize the LKC output
 if Mboot > 1
-    L_hat = zeros( [ D, Mboot ] );
+    L_hat = NaN*zeros( [ D, Mboot ] );
 else
-    L_hat = zeros( [ D, N ] );    
+    L_hat = NaN*zeros( [ D, N ] );    
+end
+
+% Apply the mask to the data, i.e., set values outside the mask to -oo
+if ~all( mask(:) )
+    mask( mask == 0 ) = -Inf;
+    Y = repmat( mask, [ ones( [ 1 D ] ), N ] ).* Y;
 end
 
 % scaling vector for integral
@@ -123,7 +220,8 @@ if( Mboot > 1 )
     % reshape and and standardize the field, such that it has unit variance
     Y = reshape( Y, prod( sY(1:end-1) ), N );
     % normalize the residuals
-    Y = ( Y - mean( Y, 2 ) ) ./ sqrt( sum( Y.^2, 2 ) );
+    Y = Y - mean( Y, 2 );
+    Y = Y ./ sqrt( sum( Y.^2, 2 ) );
 
     for i = 1:Mboot
         % get the bootstrapped process
@@ -172,13 +270,11 @@ else
     L0 = ECall{1}(1,2);
 end
 
-% close connection to CPUs
-if( state_gcp == 42 && Npar > 1 )   
-    delete(gcp)
-end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% stat summary %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Summary estimators: LKCs
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% Prepare output as a structure
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Summary estimators: LKCs as mean of the individual estimators and the
+% corresponding covariance matrix and confidence intervals
 if N > 1
     L_hatn     = mean( L_hat, 2 );
     Sigma_hat  = cov( L_hat' );
@@ -187,11 +283,11 @@ if N > 1
                          L_hatn + 1.96 * L_se_hat );
 else
     L_hatn     = L_hat;
-    Sigma_hat  = NaN;
-    L_se_hat   = NaN;
-    L_conf_hat = NaN;
+    Sigma_hat  = "Covariance matrix cannot be computed for a single field";
+    L_se_hat   = "standard error cannot be computed for a single field";
+    L_conf_hat = "Confidence intervalls can not be computed for a single field";
 end
 % Summarize output
-LKC  = struct( 'hat1', L_hat, 'hatn', L_hatn, 'Sigma_hat', Sigma_hat, ...
-               'se_hat', L_se_hat, 'conf_hat', L_conf_hat, 'L0', L0 );
+LKC  = struct(  'hatL1', L_hat, 'hatL', L_hatn, 'L0', L0, 'hatSIGMA',...
+                Sigma_hat, 'hatSE', L_se_hat, 'confInt95', L_conf_hat );
 return
