e2072eb9e2549dbc9f43bdbcf73357d3d690a6ca
Fabian Telschow <ftelschow@ucsd.edu>
2020-06-08 13:45:28 +0200

    removed because file exists

--- Test_Scripts/Test_bdry_voxels.m
+++
@@ -1,9 +0,0 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%
-%%%%    This script tests the bdry_voxels.m function
-%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% prepare workspace
-clear all
-addpath( genpath( "/home/drtea/matlabToolboxes/RFTtoolbox/" ) )
-
