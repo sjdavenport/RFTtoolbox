d1a66c9624dfde7fa92ce54850169760e4e80b1c
Fabian Telschow <ftelschow@ucsd.edu>
2020-06-11 20:39:44 +0200

    new function for visualisation of the voxel manifold

---
+++ Stats_and_Aux_Functions/Masking/visualize_bndry3D.m
@@ -0,0 +1,130 @@
+function [ ] = visualize_bndry3D( mask, resadd, types, pts_size, angle )
+% visualize_bndry( mask, resadd, types ) visualizes the resolution
+% increased boundary
+%
+%--------------------------------------------------------------------------
+% ARGUMENTS
+% Mandatory
+%   mask   this is a mandatory variable.
+%
+% Optional
+%   resadd   this is an optional parameter. Default 0.
+%   types
+%   angle
+%
+%--------------------------------------------------------------------------
+% OUTPUT
+%   Outputs an image showing the resolution increased boundary
+%--------------------------------------------------------------------------
+% DEVELOPER TODOs:
+%--------------------------------------------------------------------------
+% EXAMPLES
+% %% % Show box example all boundaries
+% visualize_bndry3D( true([4 4 4]), 1, ["yz", "xz", "xy"], 50 )
+% %% % Show box example only one boundary
+% visualize_bndry3D( true([4 4 4]), 1, "yz", 50 )
+% %% %% Spherical object
+% % Create a mask and show it
+% siz = 3;
+% dx  = 0.5;
+% [x,y,z] = meshgrid( -siz:dx:siz, -siz:dx:siz, -siz:dx:siz );
+% xvals = [x(:), y(:), z(:)]';
+% h     = reshape( GkerMV( xvals, 5 ), size(x) );
+% mask  = logical( h > 0.003 );
+% clear h
+% %% % resadd = 1
+% visualize_bndry3D( mask, 1, [ "xy", "yz", "xz" ], 20 )
+% %% % resadd = 3
+% visualize_bndry3D( mask, 3, [ "xy", "yz" ], 40 )
+%--------------------------------------------------------------------------
+% AUTHOR: Fabian Telschow  
+%--------------------------------------------------------------------------
+
+
+%% Check mandatory input and get important constants
+%--------------------------------------------------------------------------
+
+if ~length( size( mask ) ) == 3
+    error( "mask must be 3D array." );
+end
+
+
+%% Add/check optional values
+%--------------------------------------------------------------------------
+
+% This kind of code with exists is better than using nargin < xy, since
+if ~exist( 'resadd', 'var' )
+   % Default option of opt1
+   resadd = 1;
+end
+
+% Get distance between high res points
+dx = 1 / ( resadd + 1 );
+
+% Default plot all boundary values
+if ~exist( 'types', 'var' )
+   % Default option of opt1
+   types = ["xy", "xz", "yz"];
+end
+
+% Default plot all boundary values
+if ~exist( 'angle', 'var' )
+   % Default option of opt1
+   angle = [ 52 36 ];
+end
+
+% Default plot all boundary values
+if ~exist( 'pts_size', 'var' )
+   % Default option of opt1
+   pts_size = 50;
+end
+
+%% Main function  
+%--------------------------------------------------------------------------
+
+% Define a color structure
+colors = [ "b", "y", "k" ];
+spts = pts_size * [ 1.5, 1, 0.5 ];
+           
+% Get the highres mask
+mask_hr = mask_highres( mask, resadd );
+
+% Get the coordinates of the original voxels
+[m1, m2, m3] = ind2sub( size( mask ), find( mask == 1 ) );
+mask_pts = [m1, m2, m3];
+mask_voxsize = [1, 1, 1];
+
+% Get the chosen boundary type
+[ bndry, ~ ] = bndry_voxels( logical( mask_hr ), types );
+
+if length( types ) == 1
+    % bdry voxel coordinates
+    [m1, m2, m3] = ind2sub( size( mask_hr ), find( bndry == 1 ) );
+
+    figure(1), clf, hold on
+    % Plot the original voxels
+    voxel_image( mask_pts, mask_voxsize, "red", 0.65 );
+    % Plot the bdry locations
+    scatter3( m1*dx +(0.5-dx), m2*dx+(0.5-dx), m3*dx+(0.5-dx),...
+              pts_size, 'blue', 'filled');
+    
+else
+    % Initialize the figure
+    figure(1), clf, hold on
+    % Plot the boundary points for the chosen faces
+    k = 0;
+    for type = types
+        k = k+1;
+        % bdry voxel coordinates
+        [m1, m2, m3] = ind2sub( size( mask_hr ), find( bndry.(type) == 1 ) );
+        % Plot the original voxels
+        voxel_image( mask_pts, mask_voxsize, "red", 0.65 );
+        % Plot the bdry locations
+        scatter3( m1*dx +(0.5-dx), m2*dx+(0.5-dx), m3*dx+(0.5-dx),...
+                  spts(k), colors(k), 'filled' );
+    end
+end
+% Change the view angle
+view( angle )
+
+return
