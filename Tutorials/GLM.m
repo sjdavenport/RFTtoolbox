aaa4dfd1fc803088ebf785c2c7748e608ae4ce9e
Fabian Telschow <ftelschow@ucsd.edu>
2021-01-25 21:21:12 +0100

    added pointwise check

--- Tutorials/GLM.m
+++ Tutorials/GLM.m
@@ -1,3 +1,7 @@
+% voxels to test
+i = 1;
+j = 1;
+
 % Number of samples
 N = 100;
 
@@ -12,10 +16,12 @@ x = ( 1:10 ) / 10;
 [ xx, yy ] = meshgrid( x, x );
 
 % Noise field
-error = wfield( dim, N);
+error   = wfield( dim, N);
+errorpt = squeeze( error.field( i, j, : ) );
 
 % Signal
 beta = constfield( [ 2, 2 ]', true( dim ) );
+betapt = squeeze( beta.field( i, j, : ) );
 
 % Design matrix
 x1    = ones( [ 1, N ] );
@@ -25,22 +31,39 @@ x2(1:4) = 1;
 X = [ x1; x2 ]';
 
 % Data
-Y = X * beta + error;
+Y   = X * beta + error;
+Ypt = X * betapt + errorpt;
 
 % "Estimator"
 hatX = (X'*X) \ X';
 
 % Estimate
 hatbeta = hatX * Y;
+hatbetapt = hatX * Ypt;
+
+sum( abs( squeeze( hatbeta.field(i,j,:)) - hatbetapt ) )
 
 % residuals
-residuals = Y - X * hatbeta;
+residuals   = Y - X * hatbeta;
+residualspt = Ypt - X * hatbetapt;
+
+sum( abs( squeeze( residuals.field(i,j,:)) - residualspt ) )
 df        = N - rank(X);
 
-sigma2hat = sum(residuals.^2, 1) ./ df;
-se = sqrt( (c * inv(X'* X) * c') .* sigma2hat);
+sigma2hat   = sum(residuals.^2, 1) ./ df;
+sigma2hatpt = sum(residualspt.^2, 1) ./ df;
+sum( abs( squeeze( residuals.field(i,j,:)) - residualspt ) )
 
+% standard error of contrast estimate
+se   = sqrt( (c * inv(X'* X) * c') .* sigma2hat );
+sept = sqrt( (c * inv(X'* X) * c') .* sigma2hatpt );
 
-% Compute t-field and residuals
+% Compute t-field and standardized residuals
 T = c * hatbeta ./ se;
 residuals = residuals ./ sigma2hat;
+
+Tpt = c * hatbetapt ./ sept;
+residualspt = residualspt ./ sigma2hatpt;
+
+sum( abs( squeeze( T.field(i,j,:)) - Tpt ) )
+sum( abs( squeeze( residuals.field(i,j,:)) - residualspt ) )
