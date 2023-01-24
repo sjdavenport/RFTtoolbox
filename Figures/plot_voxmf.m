304f21ef5fef995cba01b24b1742b64318737c63
Fabian Telschow <fabian.telschow@hu-berlin.de>
2023-01-24 20:13:15 +0100

    basic plot function

---
+++ Figures/plot_voxmf.m
@@ -0,0 +1,70 @@
+function [ ] = plot_voxmf( field )
+% NEWFUN( var1, var2, opt1 ) is described here [...]
+%
+%--------------------------------------------------------------------------
+% ARGUMENTS
+% Mandatory
+%   var1   this is a mandatory variable.
+%   var2   this is a mandatory variable.
+%
+% Optional
+%   opt1   this is an optional parameter. Default 0.
+%
+%--------------------------------------------------------------------------
+% OUTPUT
+%   out1  this is the first output
+%   out2  this is the second output
+%--------------------------------------------------------------------------
+% DEVELOPER TODOs:
+%--------------------------------------------------------------------------
+% EXAMPLES
+%
+%--------------------------------------------------------------------------
+% AUTHOR: Fabian Telschow  
+%--------------------------------------------------------------------------
+
+
+%% Check mandatory input and get important constants
+%--------------------------------------------------------------------------
+%%%%%% HEADLINE 
+%%% SUBHEADLINE
+% Line or short paragraph comment
+
+%% Add/check optional values
+%--------------------------------------------------------------------------
+
+% This kind of code with exists is better than using nargin < xy, since
+% Parameter can be easily permuted
+if ~exist( 'opt1', 'var' )
+   % Default option of opt1
+   opt1 = 0;
+end
+
+
+%% Main function  
+%--------------------------------------------------------------------------
+
+% Get an xvals array
+[X,Y,Z] = meshgrid(field.xvals{1}, field.xvals{2}, field.xvals{3});
+Grid = [X(:), Y(:), Z(:)];
+
+% Get mask as a vector
+mask_vec = field.mask(:);
+
+% Middle points of the boxes of the voxel manifold
+mpoints = Grid(mask_vec,:,:);
+
+% Get the lengths of the voxel manifold boxes
+delta = NaN * ones([1, 3]);
+for d = 1:3
+    delta(d) =min(diff(field.xvals{d}));
+end
+
+for i = 1:size(mpoints,1)
+    plotcube(delta, mpoints(i,:,:),.8,[1 0 0]); hold on
+end
+xlim([min(X(:)), max(X(:))])
+ylim([min(Y(:)), max(Y(:))])
+zlim([min(Z(:)), max(Z(:))])
+axis square
+return
---
+++ Figures/test_voxmf.m
@@ -0,0 +1,43 @@
+clear all
+close all
+
+rng(42)
+mask = [20 20 20];
+% Get lattice data
+wn = wfield( mask, 1 );
+% Input data
+% Kernel: SepKernel object. Numeric creates a seperable isotropic Gaussian
+% kernel with FWHM kernel
+kernel_FWHM = 5;
+% Derivative type: 0/1/2 supported giving the actual field or its first or
+% second derivative
+derivtype = 0; 
+% resolution increase of the field, i.e. number of voxels added inbetween
+% each voxel
+resadd = 0;
+% lat_mask: mask the lat_data before smoothing or not
+lat_masked = true;
+% Get the params object for a convfield
+params = ConvFieldParams(kernel_FWHM*ones([1 3]),...
+                         resadd,...
+                         ceil( resadd / 2 ),...
+                         false );
+params2 = ConvFieldParams(kernel_FWHM*ones([1 3]),...
+                         resadd + 1,...
+                         ceil( (resadd + 1) / 2 ),...
+                         false );
+
+% Construct ConvField object.
+cfield = convfield( wn, params, derivtype );
+mask  = cfield.field > 0.08;
+cfield.mask = mask;
+cfield = Mask(cfield);
+
+cfield2 = convfield( wn, params2, derivtype );
+cfield2.mask = mask_highres( mask, resadd+1, ceil( (resadd + 1) / 2 ));
+
+figure(1)
+plot_voxmf( cfield )
+
+figure(2)
+plot_voxmf( cfield2 )
